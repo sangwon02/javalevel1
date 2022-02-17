61753da4e8353cfa9ef842f520d5b333c9e13782
sangwon02 <sangwon020214@gmail.com>
2022-02-17 23:30:00 +0900

    Bool에 대해서 공부함

---
+++ Boolean.java
@@ -0,0 +1,18 @@
+public class Boolean {
+    public static void main(String[] args) {
+        System.out.println("One");
+        System.out.println(1);
+
+        System.out.println(true);
+        System.out.println(false);
+
+        String foo = "fkfkfkfkkworld";
+        //밑처럼 쓸수없다. true는 reserved word이기 때문이다.
+        //String true = "HI";
+
+        System.out.println(foo.contains("world"));
+        //foo안에 world라는 글자가 존재하기에 true가 나온다.
+
+        System.out.println(foo.contains("egoing"));
+    }
+}
