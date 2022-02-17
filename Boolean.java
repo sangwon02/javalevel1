aeda2316d026f7e98e9c6520c444cf52c53ee11b
sangwon02 <sangwon020214@gmail.com>
2022-02-17 23:45:08 +0900

    Bool 공부

--- Boolean.java
+++ Boolean.java
@@ -10,7 +10,7 @@ public class Boolean {
         //밑처럼 쓸수없다. true는 reserved word이기 때문이다.
         //String true = "HI";
 
-        System.out.println(foo.contains("world"));
+        System.out.println(foo.contains("kkworld"));
         //foo안에 world라는 글자가 존재하기에 true가 나온다.
 
         System.out.println(foo.contains("egoing"));
